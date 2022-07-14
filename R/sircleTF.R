6ffc4ad2302c6750eb881d570d9dbcc9516e3190
Ariane Mora <ariane.mora@uq.net.au>
2022-07-14 13:31:57 +1000

    Updated TF code

--- R/sircleTF.R
+++ R/sircleTF.R
@@ -18,9 +18,9 @@
 ##
 ## ---------------------------
 
-#' sircleMotif
+#' sircleMotifFimo
 #'
-#' Uses dorothea and viper to predict the activity and activity change of transcription factors.
+#' Uses FIMO to predict TF binding and then finds enrichment in clusters.
 #'
 #' @param geneId Column name with the gene identifier
 #' @param fimoFilename Filename/path of the output from fimo (usually fimo.tsv)
@@ -34,8 +34,7 @@
 #' @param outputFilename Filename for the output file.
 #' @return scimo an instance of the scimo package
 #' @export
-
-sircleMotif <- function(geneId, fimoFilename, sircleRCMFilename, TFPadjCol, TFValueCol,
+sircleMotifFimo <- function(geneId, fimoFilename, sircleRCMFilename, TFPadjCol, TFValueCol,
                         pDisplayCutoff=0.05, TFInDataset=T, fimoPcol="p-value", outputDir="",
                         regLabels="RegulatoryLabels", outputFilename="SiRCle-motif.csv") {
 
@@ -52,6 +51,46 @@ sircleMotif <- function(geneId, fimoFilename, sircleRCMFilename, TFPadjCol, TFVa
   m_df <- scimo$run()  # Run the motif finding for each cluster
   scimo$u$save_df(m_df, outputFilename)
   return(scimo)
+}
+
+#' sircleMotif
+#'
+#' Uses known TF interactions from DoRoThea and then finds enrichment of TF-Target relationships in clusters.
+#'
+#' @param clusterGeneId Column name with the gene identifier (note this must match the TF ids used in DoRoTHea i.e. gene name)
+#' @param doroFile Filename/path of the output from Dorothea 
+#' @param sircleRCMFilename Filename/path of the output from sircleRCM
+#' @param TFPadjCol Column name of the padjusted value in the sircleRCMFilename file (i.e. your RNAseq, or your protein p column)
+#' @param TFValueCol Column name of  rna value for the TF (could also use the proteomics logfc)
+#' @param pDisplayCutoff Cutoff for the clusters (only returns results less than this, usually 0.05)
+#' @param TFInDataset Whether or not you require the TF to be expressed in the dataframe
+#' @param doroLevel level(s) from Dorothea (i.e. A - D) see DoRoThea for more info.
+#' @param clusters clusters to test for enrichment.
+#' @param outputFilename Filename for the output file.
+#' @param ouputDir Directory for saving
+#' @param plotOn Whether or not to plot the results
+#' @return DF of the results
+#' @export
+sircleMotif <- function(clusterGeneId, doroFile, sircleRCMFilename, TFPadjCol, TFValueCol, TargetPadjCol, TargetValueCol,
+                        RegulatoryLabel, TFInDataset=T, doroLevel=c("A"), clusters=c("MDS", "MDE", "TPDE", "TPDS"), 
+                        outputFilename="SiRCle-motif.csv", outputDir="", plotOn=T) {
+  motf <<- import("scimotf")    # Make global
+  scimo <- motf$SciMotf_Doro(doro_file=doroFile,  # fimo.tsv (here you can put in the different files 2500bp, 100bp)
+                             cluster_file=sircleRCMFilename,  # output from RCM in the first section
+                             cluster_gene_id=clusterGeneId,                     # gene identifier we have been using
+                             padj_protein=TFPadjCol,                      # p value for the TF (could also use the value from the proteomics)
+                             logfc_protein=TFValueCol,                     # rna value for the TF (could also use the proteomics logfc)
+                             padj_rna=TargetPadjCol,
+                             logfc_rna=TargetValueCol,
+                             output_dir=outputDir,                          # output directory
+                             tf_in_dataset=TFInDataset  # Whether or not you require the TF to be expressed in the dataframe
+  )          # cutoff for the clusters (only returns results less than this)
+  m_df <- scimo$run(as.list(doroLevel), rcm_clusters=as.list(clusters))  # Run the motif finding for each cluster
+  write.csv(m_df, outputFilename) 
+  if (plotOn == T) {
+    motf$plot_cluster_tf(outputFilename, save_fig=T)
+  }
+  return(as.data.frame(m_df))
 }
 
 #' sircleMotifAddInfo
