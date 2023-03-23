d7e76ddbe1bc55428f180471f523d45e0109b5a8
Christina Schmidt <christina.schmidt1@outlook.de>
2023-03-23 15:19:26 +0100

    Moved alluvial plot to sircleRCMVis.R

--- R/sircleRCMVis.R
+++ R/sircleRCMVis.R
@@ -18,32 +18,6 @@
 ##
 ## ---------------------------
 
-#' sircleRCM_MRP
-#'
-#' Computes the regulatory clustering model (RCM) using the SiRCle regulatory rules for DNA-methylation, RNAseq and Proteomics data layers (MRP).
-#'
-#' @param rnaFile Filename for your RNAseq data (results from DeSeq2 and also your normalised expression counts)
-#' @param methFile  Filename for your DNA methylation data (results from differential methylation analysis)
-#' @param proteinFile Filename/path of you Protein data (results from DeSeq2 and also your normalised expression counts)
-#' @param geneID Column name of geneId this MUST BE THE SAME in each of your protein, RNAseq and DNAmethylation files (we join on this)
-#' @param rnaValueCol Column name of RNA value in rnaFile 
-#' @param rnaPadjCol Column name of RNA p adjusted value in rnaFile 
-#' @param methValueCol Column name of Methylation difference value in methFile 
-#' @param methPadjCol Column name of Methylation p adjusted value in methFile 
-#' @param proteinValueCol  Column name of protein log fold change in proteinFile
-#' @param proteinPadjCol Column name of protein p adjusted value in proteinFile
-#' @param rnaPadjCutoff  \emph{Optional: }Padjusted cutoff for RNAseq data \strong{Default=0.05}
-#' @param rnaLogFCCutoff \emph{Optional: } LogFoldchange cutoff for RNAseq data \strong{Default=0.5}
-#' @param proteinPadjCutoff \emph{Optional: } Padjusted cutoff for Protein data \strong{Default=0.05}
-#' @param proteinValueCutoff \emph{Optional: } LogFoldchange cutoff for Protein data \strong{Default=0.3}
-#' @param methPadjCutoff \emph{Optional: } Padjusted cutoff for DNA methylation data \strong{Default=0.05}
-#' @param methDiffCutoff \emph{Optional: } DNA Methylation difference cutoff for DNA methylation \strong{Default=10}
-#' @param backgroundMethod \emph{Optional: } Background method (NEED Description for each one here) \strong{Default="P|(M&R)"}
-#' @param outputFileName \emph{Optional: } Output filename \strong{Default=SiRCle_RCM.csv}
-#' @return rcm an instance of the rcm package
-#' @export
-#'
-
 #' sirclePlot
 #'
 #' Uses scircm to and plots a circle plot of the sircle regulatory groups.
@@ -88,5 +62,61 @@ sirclePlot <- function(filename, regLabels="RegulatoryLabels", fileType="pdf") {
     theme(legend.position="none") +
     coord_equal()
   ggsave(file=paste("SiRCle_plot.", fileType, sep="" ), plot=sircle, width=10, height=8)
+}
+
+
+
+#' sircleAlluvial
+#'
+#' This function creates an alluvial plot from an input CSV to create an alluvial plot for the RCM_MRP workflow.
+#'
+#' @param filename Path to the input file
+#' @return alluvialPlt The plot generated
+#' @export
+
+sircleAlluvial_MRP <- function(filename, outputFilename="SiRCle_Alluvial.pdf") {
+  ## ------------ Setup and installs ----------- ##
+  packages <- c("alluvial")
+  install.packages(setdiff(packages, rownames(installed.packages())))
+  library(alluvial)
+  Alluvial_DF <- read.csv(filename)
+  Alluvial_DF <-Alluvial_DF[,c(1:3,6,4)]
+  Alluvial_DF[,"Frequency"]  <- as.numeric("1")
+  #Now we can make the plot. First lets try to use the alluvial command:
+  alluvialPlt <- alluvial(Alluvial_DF[,c(1:5)], freq=Alluvial_DF$Frequency,
+                          col = case_when(Alluvial_DF$Regulation == "Methylation Driven Supression (Repression of Gene Transcription)"  ~ '#AA0E37',
+                                          Alluvial_DF$Regulation == "Transcription and Processing Driven Enhancement and/or Translation and Post-translational Modification Driven Supression" ~ '#5A5A5E',
+                                          Alluvial_DF$Regulation == "Transcription and Processing Driven Enhancement" ~ '#034D89',
+                                          Alluvial_DF$Regulation == "Translation and post-translational Modification Driven Enhancement" ~ '#067734',
+                                          Alluvial_DF$Regulation == "Transcription and Processing Driven Supression" ~ '#4998F3',
+                                          Alluvial_DF$Regulation == "Translation and Post-translational Modification Driven Supression" ~ '#46D37F',
+                                          Alluvial_DF$Regulation == "Methylation Driven Enhancement (Enhancement of Gene Transcription)" ~ '#ED4367',
+                                          Alluvial_DF$Regulation == "Transcription and Processing Driven Supression and/or Translation and post-translational Modification Driven Enhancement" ~ '#D2D2D6',
+                                          Alluvial_DF$Regulation == "Methylation Driven Supression (Repression of Gene Transcription) and Translation and Post-translational Modification Driven Enhancement"~ '#FC81DC',
+                                          Alluvial_DF$Regulation == "Methylation Driven Enhancement (Enhancement of Gene Transcription) and Translation and Post-translational Modification Driven Supression"~ '#991A7E',
+                                          Alluvial_DF$Regulation == "Methylation Driven Supression of non-coding RNA"~ '#FDFB89',
+                                          Alluvial_DF$Regulation == "Methylation Driven Enhancement of non-coding RNA"~ '#FCCE3A',
+                                          TRUE ~ 'grey'),
+                          border = case_when(Alluvial_DF$Regulation == "Methylation Driven Supression (Repression of Gene Transcription)"  ~ '#AA0E37',
+                                             Alluvial_DF$Regulation == "Transcription and Processing Driven Enhancement and/or Translation and Post-translational Modification Driven Supression" ~ '#5A5A5E',
+                                             Alluvial_DF$Regulation == "Transcription and Processing Driven Enhancement" ~ '#034D89',
+                                             Alluvial_DF$Regulation == "Translation and post-translational Modification Driven Enhancement" ~ '#067734',
+                                             Alluvial_DF$Regulation == "Transcription and Processing Driven Supression" ~ '#4998F3',
+                                             Alluvial_DF$Regulation == "Translation and Post-translational Modification Driven Supression" ~ '#46D37F',
+                                             Alluvial_DF$Regulation == "Methylation Driven Enhancement (Enhancement of Gene Transcription)" ~ '#ED4367',
+                                             Alluvial_DF$Regulation == "Transcription and Processing Driven Supression and/or Translation and post-translational Modification Driven Enhancement" ~ '#D2D2D6',
+                                             Alluvial_DF$Regulation == "Methylation Driven Supression (Repression of Gene Transcription) and Translation and Post-translational Modification Driven Enhancement"~ '#FC81DC',
+                                             Alluvial_DF$Regulation == "Methylation Driven Enhancement (Enhancement of Gene Transcription) and Translation and Post-translational Modification Driven Supression"~ '#991A7E',
+                                             Alluvial_DF$Regulation == "Methylation Driven Supression of non-coding RNA"~ '#FDFB89',
+                                             Alluvial_DF$Regulation == "Methylation Driven Enhancement of non-coding RNA"~ '#FCCE3A',
+                                             TRUE ~ 'grey'),
+                          hide = Alluvial_DF$Frequency == 0,
+                          cex = 0.5,
+                          gap.width = 0.7,
+                          xw=0.1,
+                          cw=0.2
+  )
+  ggsave(file=outputFilename, plot=alluvialPlt, width=10, height=8)
+  return(alluvialPlt)
 }
 
